2cd9768df7b188029b4abb9abaf4406cdc805eee
kolobanova <alinakolobanova22@gmail.com>
2025-04-02 08:34:45 +0300

    Realizeta studentu vardu korekti

--- src/kalulators.java
+++ src/kalulators.java
@@ -17,7 +17,19 @@ public class kalulators {
 			studSk = scan.nextInt();
 		} while(studSk<1);
 		String[] studenti = new String[studSk];
-
+		
+		do {
+			System.out.println("Kads bus kriteriju skaits?");
+			while(!scan.hasNextInt()) {
+				System.out.println("Kads bus kriteriju skaits?");
+				scan.next();
+			}
+			kritSk = scan.nextInt();
+		} while(kritSk<1);
+		String[] kriteriji = new String[kritSk];
+		int[] svars = new int[kritSk];
+		int[][] vertejums = new int[studSk][kritSk];
+		double[] semestrs = new double[studSk];
 	}
 
 }
