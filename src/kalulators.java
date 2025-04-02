af7834e199bc2604bed48887fa14bda93dbb760a
kolobanova <alinakolobanova22@gmail.com>
2025-04-02 08:27:44 +0300

    Nodrosina kriteriju skaits

--- src/kalulators.java
+++ src/kalulators.java
@@ -1,8 +1,22 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class kalulators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int studSk, kritSk;
+		Scanner scan = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0.#");
+		
+		do {
+			System.out.println("Cik studentiem aprekinat gala vertejumu?");
+			while(!scan.hasNext()) {
+				System.out.println("Cik studentiem aprekinat gala vertejumu?");
+				scan.next();
+			}
+			studSk = scan.nextInt();
+		} while(studSk<1);
+		String[] studenti = new String[studSk];
 
 	}
 
